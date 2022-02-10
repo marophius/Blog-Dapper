8db7493e59fd649b58e11927aa4d56607c9e5536
Icaro Félix <ifeme08@gmail.com>
2022-02-10 20:24:25 -0300

    Tag controller finalizado

--- Desafio/BlogDapperApi/BlogDapperApi/Controllers/TagController.cs
+++ Desafio/BlogDapperApi/BlogDapperApi/Controllers/TagController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlogDapperApi.Interfaces;
+using BlogDapperApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogDapperApi.Controllers
@@ -10,6 +12,109 @@ namespace BlogDapperApi.Controllers
         public TagController()
         {
 
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Tag>> GetAll(
+            [FromServices]
+            ITagRepository repository
+            )
+        {
+            var tags = await repository.GetAll();
+
+            foreach(var tag in tags)
+            {
+                Console.WriteLine($"{tag.Id} - {tag.Name} ({tag.Slug})");
+            }
+
+            return tags;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Tag>> GetById(
+            int id,
+            [FromServices]
+            ITagRepository repository
+            )
+        {
+            try
+            {
+                if(id == 0)
+                {
+                    throw new Exception("Identificador não válido");
+                }
+
+                var user = await repository.GetById(id);
+
+                return Ok(user);
+
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tag>> AddTag(
+            [FromBody]
+            Tag tag,
+            [FromServices]
+            ITagRepository repository
+            )
+        {
+            try
+            {
+                if (tag == null)
+                {
+                    throw new Exception("Objeto nulo");
+                }
+
+                await repository.Add(tag);
+                return Ok(tag);
+
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Tag>> UpdateTag(
+            Tag tag,
+            [FromServices]
+            ITagRepository repository
+            )
+        {
+            try
+            {
+                if(tag == null)
+                {
+                    throw new Exception("Objeto nulo");
+                }
+
+                await repository.Update(tag);
+
+                return Ok(tag);
+
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public void DeleteTag(
+            int id,
+            [FromServices]
+            ITagRepository repository
+            )
+        {
+            if(id == 0)
+            {
+                throw new Exception("Identificador inválido");
+            }
+
+            repository.Delete(id);
         }
     }
 }
